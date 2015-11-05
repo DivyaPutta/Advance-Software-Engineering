cf8f5b4e39742f6648385343567d18087b4211a3
harshakomalla <harshakomalla@gmail.com>
2015-11-05 09:01:20 -0600

    commit

---
+++ source/code-9th assignment/UMKCStudentID (1)/instructions.md
@@ -0,0 +1,10 @@
+Open Getting Started Guide
+-----------------------------------------
+Congratulations on your new Mobile Backend!
+
+To use the Mobile Backend template as a starting point, and if you already have a development environment set up, get started with these simple steps:
+
+1. [download the starter application package](https://console-monolith-20151021-161140.ng.bluemix.net:443/rest/../rest/apps/f076bf18-5ecd-414b-9e3c-90ece053d4d3/starter-download)
+2. Download the [SDKs](https://www.ng.bluemix.net/docs/#starters/mobile/sdk.html)
+3. Modify the starter application package as needed
+4. Using [cf Command Line Interface](https://github.com/cloudfoundry/cli) to push back your updated application
---
+++ source/code-9th assignment/UMKCStudentID/www/js/controllers.js
@@ -0,0 +1,158 @@
+
+angular.module('starter.controllers', ['starter.services'])
+
+.controller('LoginCtrl', function($scope, LoginService,UpdateService, DeleteService,  $ionicPopup, $state) {
+    $scope.data = {};
+ 
+    $scope.login = function(username) {
+         LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('adminhome');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+           
+    }
+  /* $scope.fbLogin = function () {
+    ngFB.login({scope: 'email, public_profile,publish_actions'}).then(
+        function (response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                //$scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        });
+};*/
+    $scope.signup =function()
+    {
+        $state.go('register');
+    }
+    
+    $scope.update =function(username)
+    {
+         UpdateService.updateUser($scope.data.username, $scope.data.password).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Updated!',
+                template: 'Your account is updated succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Wrong Update!'
+            });
+        });
+}
+     $scope.delete =function(username)
+    {
+         DeleteService.deleteUser($scope.data.username, $scope.data.password).success(function(data) {
+         var alertPopup = $ionicPopup.alert({
+                title: 'Deleted!',
+                template: 'Your account is deleted succesfully!'
+            });
+    }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+         }
+    
+     
+}
+)
+.controller('RegisterCtrl', function($scope, RegisterService, $ionicPopup, $state) {
+    $scope.data = {};
+ 
+    $scope.register = function(email){
+      
+            RegisterService.RegisterUser($scope.data.firstname, $scope.data.lastname, $scope.data.address, $scope.data.age, $scope.data.email, $scope.data.username, $scope.data.password ).success(function(data) {
+           alert(data.lastname);
+                $state.go('login');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
+})
+.controller('MainCtrl', function($scope, $state) {
+    $scope.admin=function() {
+        $state.go('login');
+    }
+    $scope.student=function() {
+        $state.go('studentlogin');
+    }
+})
+.controller('adminCtrl', function($scope, $state) {
+    $scope.generateqrcode=function() {
+        $state.go('qrpage');
+    }
+})
+.controller('studenthomeCtrl', function($scope, $state, $stateParams) {
+     console.log($stateParams.username);
+    $scope.view=function() {
+       
+        
+        $state.go('view', {username: $stateParams.username} );
+    }
+})
+
+
+.controller('viewCtrl', function($scope, $state, $stateParams, $http) {
+     console.log($stateParams.username);
+   $scope.username = $stateParams.username;
+    
+    
+    
+})
+
+
+.controller('qrpageCtrl', function ($scope, $http) {
+
+      //  document.getElementById('username').innerHTML += localStorage.getItem('username');  
+     // $scope.initialize = function () {
+     //    $scope.username = localStorage.getItem('UserName'); 
+     //    console.log($scope.username);
+     //    document.getElementById('username').innerHTML = "Hi";
+     // };
+ 
+$scope.generate = function(username) {
+ document.getElementById("qrimg").src = "https://api.qrserver.com/v1/create-qr-code/?data="+document.getElementById("username").value +"&amp;size=100x100" ; 
+}
+
+/*$scope.getdetails = function () {
+$http.get('http://api.qrserver.com/v1/read-qr-code/?fileurl='+document.getElementById("qrimg").src).success(function(data) {
+       //   console.log(data[0].symbol[0].data);
+         // document.getElementById("country").innerHTML = data[0].symbol[0].data;
+
+         $http.get('http://api.zippopotam.us/us/'+document.getElementById("username").value).success(function(data) {
+            console.log(data);
+            document.getElementById("country").innerHTML = data.country;
+            document.getElementById("place").innerHTML = data.places[0]['place name'];
+            document.getElementById("state").innerHTML = data.places[0].state;
+            document.getElementById("lat").innerHTML = data.places[0].latitude;
+            document.getElementById("lng").innerHTML = data.places[0].longitude;
+         })                     
+//})
+};*/
+
+
+
+})
+.controller('studentloginCtrl', function($scope, StudentLoginService, $ionicPopup, $state) {
+    $scope.data = {};
+ 
+    $scope.studentlogin = function(username) {
+         StudentLoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('studenthome', {username: $scope.data.username});
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }});
